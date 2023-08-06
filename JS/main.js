b630be7b1fa13795456eca75bff2cb2c70b107e8
Fernando Saucedo <frsauce@gmail.com>
2023-08-06 01:33:42 -0300

    se agrega js

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,16 @@
-console.log('holi');
+let ul = document.getElementById("ul");
+let input = document.getElementById("input");
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click",function(e) {
+
+    e.preventDefault();
+
+    let li = document.createElement("li");//se crea la variable li para crear el elemento contenedor de la entrada del input
+
+    li.innerHTML = input.value// al contenedor li se le asigna el valor del input
+
+    ul.appendChild(li); //se agrega al elemento html
+
+    input.value = "";
+});
